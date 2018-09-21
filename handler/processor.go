7084ec2a0a898cb7994b45a39d7fcb16ebba9adb
Nabin Kharal <nabin@whitehatengineering.com>
2018-09-21 16:31:51 +0545

    parsing and storing to Request obj complete almost

--- handler/processor.go
+++ handler/processor.go
@@ -25,9 +25,19 @@ func init() {
 func processSearch() {
 	log.Println("processing the info for all the available sites ..... ")
 	for _, site := range sites {
+		fmt.Println("selected site: ", site)
 		info := getURL(site, searchRequest.JobTitle)
 		if searchRequest.JobTitle != "any" {
-			fetch.MakeGet(info.SearchURL)
+			pageSource := fetch.MakeGet(info.SearchURL)
+			fmt.Println("fetch page completed for page: ", info.SearchURL)
+			links := parser.ParseSearchPage(pageSource, site)
+			fmt.Println("the links from search are", links)
+			for _, link := range links {
+				pgsrc := fetch.MakeGet(link)
+				fmt.Println("fetch page completed for page: ", link)
+				parser.ParseVacancyPage(pgsrc, site)
+			}
+			fmt.Println("parsing completed")
 		} else if searchRequest.JobCategory != "any" {
 			pageSource := fetch.MakeGet(info.SearchByCategory)
 			parser.ParseCategoryPage(pageSource, site)
@@ -40,7 +50,7 @@ func getURL(site string, searchString string) SiteInfo {
 	var URL SiteInfo
 	switch site {
 	case "merojob":
-		URL.SearchURL = fmt.Sprintf("https://merojob.com/?q=%s", searchString)
+		URL.SearchURL = fmt.Sprintf("https://merojob.com/search/?q=%s", searchString)
 		URL.SearchByCategory = fmt.Sprintf("https://merojob.com/category/")
 		URL.SearchByJobTitle = fmt.Sprintf("https://merojob.com/designation")
 		URL.SearchByOrganization = fmt.Sprintf("https://merojob.com/company")
--- parser/parser.go
+++ parser/parser.go
@@ -1,10 +1,91 @@
 package parser
 
-func init() {
+import (
+	"fmt"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Request struct {
+	Experience    string `json:"experience"`
+	Salary        string `json:"salary"`        // 40000
+	Qualification string `json:"qualification"` //bachelor in CSIT
+	Location      string `json:"location"`      //  Pokhara
+	JobTitle      string `json:"title"`         //  Accountant
+	JobCategory   string `json:"category"`      // IT and Telecommunications
+	JobType       string `json:"type"`          // full time , part time
+	JobLevel      string `json:"level"`         // senior, junior, mid
 }
 
+var retrivedData Request
+
+//ParseCategoryPage ...
 func ParseCategoryPage(pageSource string, site string) []string {
 	var arr []string
 	return arr
+}
+
+// ParseSearchPage ...
+func ParseSearchPage(pageSource string, site string) []string {
+	var resultArray []string
+	p := strings.NewReader(pageSource)
+	doc, _ := goquery.NewDocumentFromReader(p)
+	doc.Find(".card-block").Each(func(i int, s *goquery.Selection) {
+		resultUrl, found := s.Find(".col-lg-9").Find("h1").Find("a").Attr("href")
+		if found {
+			resultArray = append(resultArray, fmt.Sprintf("http://%s.com%s", site, resultUrl))
+		}
+	})
+	return resultArray
+}
+
+func ParseVacancyPage(pgsrc string, site string) {
+	p := strings.NewReader(pgsrc)
+	doc, _ := goquery.NewDocumentFromReader(p)
+	doc.Find(".card-group").Each(func(i int, s *goquery.Selection) {
+		_, exists := s.Find(".card-header").Find("h3").Attr("class")
+		if exists {
+			header := s.Find(".card-header").Find("h3")
+			if strings.Contains(header.Text(), "Specification") {
+				// parse the inside of the specification for particular website
+				s.Find(".card-block").Find("table").Find("tr").Each(func(i int, s *goquery.Selection) {
+					s.Children().Each(func(j int, t *goquery.Selection) {
+						// fmt.Println(j, t.Text())
+						if j == 0 {
+							saveData(t)
+						}
+					})
+				})
+			} else if strings.Contains(header.Text(), "Information") {
+				// parse the inside of information for a particular website
+				s.Find(".card-block").Find("table").Find("tr").Each(func(i int, s *goquery.Selection) {
+					s.Children().Each(func(j int, t *goquery.Selection) {
+						// fmt.Println(j, t.Text())
+						if j == 0 {
+							saveData(t)
+						}
+					})
+				})
+			}
+		}
+	})
+	fmt.Println("the data retrived from website is", retrivedData)
+}
+func saveData(t *goquery.Selection) {
+	nextVal := strings.Trim(t.Next().Text(), " ")
+	// save data to request object
+	if strings.Contains(strings.ToLower(t.Text()), "category") {
+		retrivedData.JobCategory = nextVal
+	} else if strings.Contains(strings.ToLower(t.Text()), "level") {
+		retrivedData.JobLevel = nextVal
+	} else if strings.Contains(strings.ToLower(t.Text()), "location") {
+		retrivedData.Location = nextVal
+	} else if strings.Contains(strings.ToLower(t.Text()), "salary") {
+		retrivedData.Salary = nextVal
+	} else if strings.Contains(strings.ToLower(t.Text()), "education") {
+		retrivedData.Qualification = nextVal
+	} else if strings.Contains(strings.ToLower(t.Text()), "experience") {
+		retrivedData.Experience = nextVal
+	}
 }
